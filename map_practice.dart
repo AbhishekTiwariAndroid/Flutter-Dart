16802944720aaff9e708ecdff3d6fc998a753283
Abhishek Tiwari <42689087+AbhishekTiwariAndroid@users.noreply.github.com>
2023-05-16 14:55:23 +0530

    Update map_practice.dart key2 print

--- map_practice.dart
+++ map_practice.dart
@@ -13,7 +13,7 @@ void main() {
   map['Key1'] = 'Hello';  // because K is capital and didn't matches the it will add it output - {key1: Abhishekdev, key2: 4, Key1: Hello}
   
   print(map);
-  print(map['key2']); // key
+  print(map['key2']); // key output - 4
   print(map['key3']); // if key not exist then it will print null
   
   
