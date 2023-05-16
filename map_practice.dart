6aa41387ad6a1ccb6035a3cc5d4d8aadd947a965
Abhishek Tiwari <42689087+AbhishekTiwariAndroid@users.noreply.github.com>
2023-05-16 14:47:49 +0530

    Update map_practice.dart override mapkey

--- map_practice.dart
+++ map_practice.dart
@@ -8,6 +8,9 @@ void main() {
   };
   
   
+  map['key1'] = 'Abhishekdev'; // it will override previous given value to it with this
+  
+  
   print(map);
   print(map['key2']); // key
   print(map['key3']); // if key not exist then it will print null
