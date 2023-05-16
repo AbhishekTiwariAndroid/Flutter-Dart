70da59012ac2416bca255053de483b470d474b14
Abhishek Tiwari <42689087+AbhishekTiwariAndroid@users.noreply.github.com>
2023-05-16 14:35:00 +0530

    Update map_practice.dart null for not exist key

--- map_practice.dart
+++ map_practice.dart
@@ -1,17 +1,17 @@
 void main() {
   
   
-   
   var map = {
     'key1':'value',
-    'key2':'2'
+    'key2':'4'
 
   };
   
- 
+  
   print(map);
-  print(map['key2']);  // to get key of choice
+  print(map['key2']); // key
+  print(map['key3']); // if key not exist then it will print null
   
- 
   
+
 }
